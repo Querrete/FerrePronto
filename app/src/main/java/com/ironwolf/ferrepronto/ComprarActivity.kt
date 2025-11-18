90b9d61d5fd5e8077aedfb40cdc6955990447309
wronskiano183 <146133857+wronskiano183@users.noreply.github.com>
2025-11-18 17:43:36 -0600

    se agregaron boton regresar y vista
    se agrego el boton para regrear y el ruteo de la vista para que vaya a la correcta

--- app/src/main/java/com/ironwolf/ferrepronto/ComprarActivity.kt
+++ app/src/main/java/com/ironwolf/ferrepronto/ComprarActivity.kt
@@ -1,6 +1,8 @@
 package com.ironwolf.ferrepronto
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,10 @@ class ComprarActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_comprar)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val regresarhome = findViewById<TextView>(R.id.regresarhome)
+        regresarhome.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
