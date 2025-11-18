48e563fa57c05f158e24a5a2cf0cdcdfcd9632df
Querrete <querrete12@hotmail.com>
2025-11-18 16:47:36 -0600

    Mapa funcional
    Se agrego un mapa funcional sin api, es mucho pedo para la api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+
 }
--- app/src/main/java/com/ironwolf/ferrepronto/UbicacionActivity.kt
+++ app/src/main/java/com/ironwolf/ferrepronto/UbicacionActivity.kt
@@ -1,21 +1,33 @@
 package com.ironwolf.ferrepronto
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class UbicacionActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_ubicacion) // crea este XML si no existe
+        setContentView(R.layout.activity_ubicacion)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // ---- WebView ----
+        val webView = findViewById<WebView>(R.id.webviewMapa)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        val url = "https://www.google.com/maps/d/u/0/embed?mid=1EyRNa3hGRukZHXTXRFqcHZLhMNopK4E&ehbc=2E312F"
+        webView.loadUrl(url)
+
+        // ---- BOTÓN PARA REGRESAR ----
+        val regresarhome = findViewById<TextView>(R.id.regresarhome)
+        regresarhome.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
